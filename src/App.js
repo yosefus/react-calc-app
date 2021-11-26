aea91c3c8ad8641a31a25db88b6f84d2b2eb4cef
yosefus <yosalsoni@gmail.com>
2021-11-26 14:46:50 +0200

    new upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calc from './components/Calc';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <Calc />
+      <Footer />
     </div>
   );
 }
